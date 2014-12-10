61c60f1e90ccd02224cf06cf59a0c686f35734a7
Yao Hao <yhaoao@163.com>
2014-12-10 18:35:40 +0800

    add screenshot

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ngSJM
 =====
 
 使用Angular开发神经猫游戏
+
+![Singleton](https://rawgit.com/mgechev/angularjs-in-patterns/master/images/singleton.svg "Fig. 1")
--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
 angular.module('ngSJM', ['ngSJM.services', 'ngSJM.controllers', 'ngSJM.directives']).config(function() {
 
 }).run(function($window, Env) {
-    // var ratio = $window.innerWidth / 500;
-    // ratio = ratio < 1 ? ratio : 1;
-    // var styleElm = document.createElement('style');
-    // styleElm.innerHTML =
-    //     '.scale-x{ -webkit-transform: scale(' + ratio + ', 1);-webkit-transform-origin: bottom left;}' +
-    //     '.scale-y{ -webkit-transform: scale(1, ' + ratio + ');-webkit-transform-origin: bottom bottom;}';
-    // $window.document.querySelector('head').appendChild(styleElm);
     Env.init();
 });
